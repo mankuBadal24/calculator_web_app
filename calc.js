ecbb8814e076c46d8ccd2b161ada78913d10333d
mankuBadal24 <144877956+mankuBadal24@users.noreply.github.com>
2024-07-10 10:44:06 +0530

    updates

--- calc.js
+++ calc.js
@@ -1,9 +1,9 @@
-function add(x){
-    document.getElementById("out").value += x; 
+function add(x) {
+  document.getElementById("out").value += x;
 }
-function cal(){
-    var output = document.getElementById("out").value;
-    var result = eval(output);
-    document.getElementById("out").value = result;
-    document.getElementById("out").value = result;
+function cal() {
+  var output = document.getElementById("out").value;
+  var result = eval(output);
+  document.getElementById("out").value = result;
+  document.getElementById("out").value = result;
 }
